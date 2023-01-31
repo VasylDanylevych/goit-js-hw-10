fee4b0b537664a5f19396d127e13b966c3a67744
VasylDanylevych <ukrainec31@gmail.com>
2023-01-31 03:19:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ function inputValue(e) {
     const valueOnInpud = input.value.trim();
         fetchCountries(valueOnInpud)
         .then((countries) => {
+            // console.log(countries)
             if (countries.length > 10) 
             return Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
             if (countries.length >= 2 && countries.length <= 10)
@@ -39,11 +40,9 @@ function createCardOfCountry(country) {
     <img class="flag-img" src="${country.flags.svg}"><h2>"${country.name.common}"</h2>
     <p><strong>"Capital:</strong> ${country.capital[0]}"</p>
     <p><strong>"Population:</strong> ${country.population}"</p>
-    <p><strong>"Languages:</strong> ${country.languages}"</p>
+    <p><strong>"Languages:</strong> ${Object.values(country.languages).join(', ')}"</p>
     `
 };
-
-
 
 function updateNewsList(markup) {
     document.querySelector(".country-list").innerHTML = markup;
