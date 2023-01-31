d9ab53b4cf2722820075378357130fc5d2e7d51e
VasylDanylevych <ukrainec31@gmail.com>
2023-01-31 17:06:29 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -15,26 +15,39 @@ input.addEventListener("input", debounce(inputValue, DEBOUNCE_DELAY));
 
 function inputValue(e) {
     const input = e.target;
-    const valueOnInpud = input.value.trim();
-        fetchCountries(valueOnInpud)
+    const valueOnInput = input.value.trim();
+    if (valueOnInput === "") {     
+        countryList.innerHTML = "";
+        countryInfo.innerHTML = "";   
+        return
+    } else {
+        fetchCountries(valueOnInput)
         .then((countries) => {
             // console.log(countries)
-            if (countries.length > 10) 
-            return Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+            if (countries.length > 10) {
+                countryList.innerHTML = "";
+                countryInfo.innerHTML = "";
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+                return
+            }  
 
             if (countries.length >= 2 && countries.length <= 10) {
-                return countries.reduce((markup, country) => createListOfCountries(country) + markup, "")
-            } else countryList.innerHTML = "";
-           
-            
+                const countryListMarkup = countries.reduce((markup, country) => createListOfCountries(country) + markup, "")
+                updateList(countryListMarkup)
+                countryInfo.innerHTML = "";
+                return
+            }  
+
             if (countries.length = 1) {
-                return countries.reduce((markup, country) => createCardOfCountry(country) + markup, "");
-            } else countryList.innerHTML = "";
-            
+                const countryCardMarkup =  countries.reduce((markup, country) => createCardOfCountry(country) + markup, "");
+                updateInfo(countryCardMarkup);
+                countryList.innerHTML = "";
+                return
+            }  
+
         })
-        .then(updateList)
         .catch(onError);
-        
+    }
 };
 
 function createListOfCountries(country) {
@@ -46,23 +59,25 @@ function createListOfCountries(country) {
 
 function createCardOfCountry(country) {
     return `
-    <li class = "country-info-item">
+    <div class = "country-info-item">
         <div class = "country-item"><img class="flag-img" src="${country.flags.svg}"><h2>${country.name.common}</h2></div>
         <p><strong>Capital:</strong> ${country.capital}</p>
         <p><strong>Population:</strong> ${country.population}</p>
         <p><strong>Languages:</strong> ${Object.values(country.languages).join(", ")}</p>
-    </li>`
+    </div>`
 };
 
 function updateList(markup) {
     countryList.innerHTML = markup;
+
 };
 
-// function updateInfo(markup) {
-//     countryInfo.innerHTML = markup;
-// };
+function updateInfo(markup) {
+    countryInfo.innerHTML = markup;
+
+};
 
 
-function onError(err) {
+function onError() {
     Notiflix.Notify.failure("Oops, there is no country with that name")
 };
